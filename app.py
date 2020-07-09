f85ec364125b4cc9db89e917bacb0b0a3ad9498c
santhozkumar <51332144+santhozkumar@users.noreply.github.com>
2020-07-10 02:31:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import numpy as np
 import re, string
 import pickle
 import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 import sys
